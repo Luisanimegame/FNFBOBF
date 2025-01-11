dcc888ec69c0d38b2f991191faea0f3ea6ed0113
Bonny Wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2025-01-11 00:21:42 -0300

    Meow

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1433,10 +1433,7 @@ class PlayState extends MusicBeatState
 	public function reloadHealthBarColors() {
 		healthBar.createFilledBar(FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]),
 			FlxColor.fromRGB(boyfriend.healthColorArray[0], boyfriend.healthColorArray[1], boyfriend.healthColorArray[2]));
-			
-		timeBar.createGradientBar(FlxColor.TRANSPARENT(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]),
-			FlxColor.TRANSPARENT(boyfriend.healthColorArray[0], boyfriend.healthColorArray[1], boyfriend.healthColorArray[2]));
-
+			 
 		healthBar.updateBar();
 	}
 
