cdd8f24d8feda7f20e987c1db28f39b083a8ea53
Bonny Wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2025-01-14 16:18:35 -0300

    Meow

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -158,18 +158,24 @@ class MainMenuState extends MusicBeatState
 			menuItem.antialiasing = ClientPrefs.globalAntialiasing;
 			//menuItem.setGraphicSize(Std.int(menuItem.width * 0.58));
 			menuItem.updateHitbox();
-			
-			descText = new FlxText(50, FlxG.height + offsetThing - 25, 1180, "", 32);
-			descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER/*, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK*/);
-			descText.scrollFactor.set();
-			//descText.borderSize = 2.4;
-			descBox.sprTracker = descText;
-			add(descText);
-			
-			descText.text = optionShit[i];
 		}
 
 		FlxG.camera.follow(camFollowPos, null, 1);
+		
+		descBox = new AttachedSprite();
+		descBox.makeGraphic(1, 1, FlxColor.BLACK);
+		descBox.xAdd = -10;
+		descBox.yAdd = -10;
+		descBox.alphaMult = 0;
+		descBox.alpha = 0;
+		add(descBox);
+		
+		descText = new FlxText(50, FlxG.height + offsetThing - 25, 1180, "", 32);
+		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER/*, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK*/);
+		descText.scrollFactor.set();
+		//descText.borderSize = 2.4;
+		descBox.sprTracker = descText;
+		add(descText);
 
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "FNF' BF TAKEOVER V1", 12);
 		versionShit.scrollFactor.set();
@@ -319,6 +325,7 @@ class MainMenuState extends MusicBeatState
 		if (curSelected < 0)
 			curSelected = menuItems.length - 1;
 			
+		descText.text = optionShit[i];
 		descText.y = FlxG.height - descText.height + offsetThing - 60;
 
 		menuItems.forEach(function(spr:FlxSprite)
